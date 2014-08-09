450ffba1c051c34b517c7a6c30c782b291e75ee9
Rahul Ramakrishnan <rahul@plaid.com>
2014-08-08 17:55:40 -0700

    relinked mongo to server

--- config/mongo-config.js
+++ config/mongo-config.js
@@ -1,6 +1,6 @@
 var domain = 'mongodb://';
-var user = 'rahul';
-var password = 'SharkTank63$';
-var extension = '@kahana.mongohq.com:10089/fire_logs';
+var user = 'cecile';
+var password = 'MONG4me0nweb';
+var extension = '@kahana.mongohq.com:10079/fire_logs';
 
 exports.dbURL = domain + user + ':' + password + extension;
